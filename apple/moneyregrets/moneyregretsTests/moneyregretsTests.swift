2ab2241e4a65c49e5fccb9b2fb6a9a1ec489c10a
Nathan C. McGuire <nathan@ncm.me>
2025-05-22 23:04:08 -0500

    xcode project

---
+++ apple/moneyregrets/moneyregretsTests/moneyregretsTests.swift
@@ -0,0 +1,16 @@
+//
+//  moneyregretsTests.swift
+//  moneyregretsTests
+//
+//  Created by Nathan McGuire on 5/22/25.
+//
+
+import Testing
+
+struct moneyregretsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
