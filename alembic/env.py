294519046ee476152653b0cae65812b922880534
Nathan C. McGuire <nathan@ncm.me>
2025-05-27 07:08:22 -0500

    Alembig updates

--- alembic/env.py
+++ alembic/env.py
@@ -34,7 +34,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 
-from money_regrets_api.models import *
+from api.models import *
 target_metadata = SQLModel.metadata
 
 
